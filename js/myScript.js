188992df373c530d2e6271bf468fff77e03d83c4
unknown <procode@procode.eu>
2020-09-03 19:46:38 +0200

    use of indicators to find elements on DOM

--- js/myScript.js
+++ js/myScript.js
@@ -9,6 +9,22 @@ class SpeedSetter {
         this.changeSpeedBtn;
         this.speedOptions;
 
+        this.usableDomTreeIndicators = {
+            classList: 'classList',
+            id: 'id',
+            role: 'role'
+        };
+
+        this.domTreeElementsIndicators = {
+            settingsBtn: {
+                classList: ['ytp-settings-button']
+            },
+            changeSpeedMenuItem: {
+                classList: ['ytp-menuitem'],
+                role: 'menuitem'
+            }
+        }
+
         this.initSettings = [
             {
                 channelName: 'codedamn',
@@ -21,107 +37,71 @@ class SpeedSetter {
                 latelySetSpeed: '0.25'
             }
         ];
-
-    
-
     }
+
+    getDOMNode(element) {
+        var elementIndicators = this.domTreeElementsIndicators[element];
+        var elementIndicatorsArray = Object.keys(elementIndicators);
+
+        var querySelector = '';
+
+        elementIndicatorsArray.forEach(
+            (indicator) => {
+                if (indicator == this.usableDomTreeIndicators.classList) {
+                    querySelector += `.${this.domTreeElementsIndicators[element].classList}`
+                } else if (indicator == this.usableDomTreeIndicators.id) {
+                    querySelector += `#${this.domTreeElementsIndicators[element].id}`
+                } else if (indicator == this.usableDomTreeIndicators.role) {
+                    console.log('in role');
+                    querySelector += `[role="${this.domTreeElementsIndicators[element].role}"]`
+                }
+            }
+        );
+
+        console.log(querySelector);
+
+        return document.querySelector(querySelector);
+    }
+
+    checkIfStringValueInIterableObject(value, object) {
+        var i;
+
+        for (i = 0; i < object.length; i++) {
+            if (value == object[i]) return true;
+        }
+
+        return false;
+    }
+
 
     clickSettingsIcon () {
+        const btn = this.getDOMNode('settingsBtn');
+        btn.click();
 
         setTimeout(() => {
-            this.player = document.querySelectorAll('#player-theater-container');
-    
-            this.player = this.player[0];
-            alert(this.player.length);
             
-            if (this.player) {
-                alert('in o00');
-                setTimeout(() => {
-                    this.buttons = this.player.querySelector('.ytp-chrome-bottom');
-        
-                    // var insertedNodes = ['asd'];
-                    // var observer = new MutationObserver(function(mutations) {
-                    //     mutations.forEach(function(mutation) {
-                    //         console.log(mutation);
-                    //         for (var i = 0; i < mutation.addedNodes.length; i++)
-                    //             insertedNodes.push(mutation.addedNodes[i]);
-                    //     })
-                    // });
-                    // observer.observe(this.buttons, {
-                    //     attributes: true,
-                    //     childList: true,
-                    //     subtree: true,
-                    //     characterData: true
-                    // });
-            
-            
-                
-                    this.settingsBtn = this.buttons.querySelector('.ytp-settings-button');
-                    this.settingsBtn.click();
-            
-                    alert('in o0');
-                    this.openChangeSpeedMenu();
-                }, 500);
-           
-            }
-        }, 500)
-        
+        }, 3000)
 
-    }
-
-    openChangeSpeedMenu() {
-        alert('in o');
-        this.changeSpeedBtn = document.querySelectorAll('.ytp-menuitem');
-        // alert(this.changeSpeedBtn.length);
-    
-        this.changeSpeedBtn = this.changeSpeedBtn[1];
-        // alert(this.changeSpeedBtn.toString());
-        // alert(typeof this.changeSpeedBtn);
-        this.changeSpeedBtn.click();
-    
-        this.choose150();
+        this.changeSpeedBtn = this.getDOMNode('changeSpeedMenuItem');
+            this.changeSpeedBtn.click();
     }
 
     choose150() {
-        alert('in 1');
         this.speedOptions = document.querySelectorAll('.ytp-panel');
-        // alert(this.speedOptions.length);
         if (this.speedOptions.length == 2) {
-            alert('in 2');
             if (this.speedOptions) this.speedOptions = this.speedOptions[1];
-            // alert(this.speedOptions.toString());
     
             var items = this.speedOptions.querySelectorAll('.ytp-menuitem');
-            // alert(items.length);
     
             var random = Math.floor(Math.random() * 8);
     
             items[random].click();
     
-            alert('all set' + random);
         } 
     }
 }
 
 var speedSetter = new SpeedSetter();
 
+
 speedSetter.clickSettingsIcon();
-
-
-// <div class="ytp-menuitem" aria-haspopup="true" role="menuitem" tabindex="0">
-//     <div class="ytp-menuitem-icon"></div>
-//     <div class="ytp-menuitem-label">Prędkość odtwarzania</div>
-//     <div class="ytp-menuitem-content">Normalna</div>
-// </div>
-
-
-// var o = new MutationObserver(function(ms) {
-//     console.log(ms);
-// });
-
-// o.observe(document, {
-//     childList: true,
-//     attributes: true,
-//     characterData: true,
-//     characterDataOldValue: true
-// });
